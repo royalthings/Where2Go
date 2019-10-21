8ab06fcd8a9c3a02c1552742be48019693694b30
Doombringer <royalthings2019@gmail.com>
2019-10-21 10:54:05 +0300

    Initial Commit

---
+++ Where2GoTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Where2GoTest
+//
+//  Created by Дмитрий Ага on 10/21/19.
+//  Copyright © 2019 Дмитрий Ага. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
